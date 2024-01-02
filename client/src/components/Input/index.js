9fcaa20a15b4d860ce8fe07f6da691d9481e4cc8
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-03 01:37:48 +0530

    Input field:-create

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 
- const Input = () => {
+ const Input = ({
+    label = '',
+    name = '',
+    type = 'text',
+    className = '',
+    isRequired = false,
+    placeholder = ''
+ }) => {
     return(
         <div>
-            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                First className
+            <label for={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                {label}
             </label>
-            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm 
-            rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-50
+            <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm 
+            rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
             dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-50
-            dark:focus:border-blue-500 " placeholder="John" required/>
+            dark:focus:border-blue-500 ${className}`} placeholder={placeholder} required={isRequired}/>
         </div>
 
     )
--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -1,8 +1,11 @@
+import Input from "../../components/Input";
+
 const Form = () => {
   return (
     <div className="bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
       <div className=" text-4xl font-extrabold">Welcome</div>
       <div className=" text-xl font-light">Sign up to now to get started</div>
+      <Input/>
     </div>
   );
 };
