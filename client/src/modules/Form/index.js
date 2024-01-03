622116bd10079c3c13f499665be5516f43923cc1
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-03 22:53:56 +0530

    create form

--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -4,8 +4,28 @@ const Form = () => {
   return (
     <div className="bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
       <div className=" text-4xl font-extrabold">Welcome</div>
-      <div className=" text-xl font-light">Sign up to now to get started</div>
-      <Input/>
+      <div className=" text-xl font-light mb-14">
+        Sign up to now to get started
+      </div>
+      <Input
+        label="Full name"
+        name="name"
+        placeholder="Enter your name"
+        className="mb-6"
+      />
+      <Input
+        label="Email address"
+        name="email"
+        placeholder="Enter your email"
+        className="mb-6"
+      />
+      <Input
+        label="Password"
+        type="password"
+        name="password"
+        placeholder="Enter your name"
+        className="mb-6"
+      />
     </div>
   );
 };
