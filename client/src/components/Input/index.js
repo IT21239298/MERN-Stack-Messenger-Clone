81b940f2a2eb26b0d5db60b31d68ff9c52514b4e
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-03 22:48:51 +0530

    Update input Style

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,24 +1,31 @@
 import React from "react";
 
- const Input = ({
-    label = '',
-    name = '',
-    type = 'text',
-    className = '',
-    isRequired = false,
-    placeholder = ''
- }) => {
-    return(
-        <div>
-            <label for={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                {label}
-            </label>
-            <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm 
-            rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
-            dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-50
-            dark:focus:border-blue-500 ${className}`} placeholder={placeholder} required={isRequired}/>
-        </div>
-
-    )
- }
- export default Input;
+const Input = ({
+  label = "",
+  name = "",
+  type = "text",
+  className = "",
+  isRequired = false,
+  placeholder = "",
+}) => {
+  return (
+    <div>
+      <label
+        for={name}
+        className="block mb-2 text-sm font-medium text-gray-800"
+      >
+        {label}
+      </label>
+      <input
+        type={type}
+        id={name}
+        className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm 
+            rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+            dark:focus:border-blue-500 ${className}`}
+        placeholder={placeholder}
+        required={isRequired}
+      />
+    </div>
+  );
+};
+export default Input;
