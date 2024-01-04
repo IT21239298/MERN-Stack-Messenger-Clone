0b4568db752f50559a6eab752bcfe852324706e3
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-04 23:57:01 +0530

    style

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -20,7 +20,7 @@ const Input = ({
         type={type}
         id={name}
         className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm 
-            rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full px-5 py-2.5
+            rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full px-16 py-2.5
             dark:focus:border-blue-500 ${className}`}
         placeholder={placeholder}
         required={isRequired}
