22db5eec5abd4492bed18e741496ea0e9d7006b5
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-04 23:41:45 +0530

    update button style

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-const Button = ({ type = "button" }) => {
+const Button = ({
+  label = "Button",
+  type = "button",
+  className = "",
+  disabled = false,
+}) => {
   return (
-    <button type={type} className={`text-white bg-primary hover:bg-primary focus:ring-4 focus-:outline-none 
-    focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center `}>
-      {" "}
+    <button
+      type={type}
+      className={`text-white bg-primary hover:bg-primary focus:ring-4 focus-:outline-none 
+    focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ${className} `}
+      disabled={disabled}
+    >
+      {label}
     </button>
   );
 };
