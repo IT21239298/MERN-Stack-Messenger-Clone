dad91771263e7b4037047bc81a18a6a3a8c2e297
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-06 13:20:21 +0530

    sytles  login page

--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -1,21 +1,25 @@
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 
-const Form = ({
-  isSignInPage= true,
- }) => {
+const Form = ({ isSignInPage = true }) => {
   return (
     <div className="bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
-      <div className=" text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
-      <div className=" text-xl font-light mb-14">
-        Sign up to now to get started
+      <div className=" text-4xl font-extrabold">
+        Welcome {isSignInPage && "Back"}
       </div>
-      <Input
-        label="Full Name"
-        name="name"
-        placeholder="Enter your name"
-        className="mb-6"
-      />
+      <div className=" text-xl font-light mb-14">
+        {isSignInPage
+          ? "Sign up to now to get started"
+          : "Sign up to get started"}
+      </div>
+      {isSignInPage && (
+        <Input
+          label="Full Name"
+          name="name"
+          placeholder="Enter your name"
+          className="mb-6"
+        />
+      )}
       <Input
         label="Email Address"
         name="email"
@@ -29,10 +33,16 @@ const Form = ({
         placeholder="Enter your name"
         className="mb-14"
       />
-      <Button label="Sign up" className="w-1/2 mb-2" />
-      <div>Alredy have an account? <span className="text-primary cursor-pointer underline">Sign in</span></div>
-
-
+      <Button
+        label={isSignInPage ? "Sign in" : "Sign up"}
+        className="w-1/2 mb-2"
+      />
+      <div>
+        {isSignInPage ? "Didn't havean account?" : "Alredy have an account?"}
+        <span className="text-primary cursor-pointer underline">
+          {isSignInPage ? "Sign up" : "Sign in"}
+        </span>
+      </div>
     </div>
   );
 };
