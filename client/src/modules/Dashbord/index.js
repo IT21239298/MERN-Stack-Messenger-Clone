eb6a01e23b57fd112e1432fa831fb8d549083329
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-08 23:49:40 +0530

    separate three row

--- client/src/modules/Dashbord/index.js
+++ client/src/modules/Dashbord/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Dashbord = () => {
-  return <div>Dashbord</div>;
+  return (
+    <div className="w-screen flex">
+        <div className="w-[25%] border border-black h-screen bg-secondary"></div>
+        <div className="w-[50%] border border-black h-screen"> </div>
+        <div className="w-[25%] border border-black h-screen"> </div>
+    </div>
+   
+  )
+  
 };
 
 export default Dashbord;
