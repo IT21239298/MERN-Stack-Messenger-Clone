f2a13e6caac95eba663e365ff59b7b95d54715a4
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-08 23:32:15 +0530

    create dashbord

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './modules/Form';
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./modules/Form";
+import Dashbord from "./modules/Dashbord";
 
 function App() {
   return (
-   <div className="bg-[#edf3fc] h-screen flex justify-center items-center">
-   
-     <Form/>
-   </div>
+    <div className="bg-[#edf3fc] h-screen flex justify-center items-center">
+      <Dashbord />
+      {/* <Form/> */}
+
+      
+    </div>
   );
 }
 
