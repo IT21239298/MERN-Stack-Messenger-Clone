991ca19415b5efe85365c0db6a935f52bdaf6e4d
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-08 23:56:02 +0530

    profile fontend

--- client/src/modules/Dashbord/index.js
+++ client/src/modules/Dashbord/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Avatar from "../../assets/avatar.svg"
 
 const Dashbord = () => {
   return (
     <div className="w-screen flex">
-        <div className="w-[25%] border border-black h-screen bg-secondary"></div>
+        <div className="w-[25%] border border-black h-screen bg-secondary">
+            <div>
+                <img src=""/>
+                <div>
+                    <h3>Tutorial Dv</h3>
+                    <p>my account</p>
+                </div>
+            </div>
+        </div>
         <div className="w-[50%] border border-black h-screen"> </div>
         <div className="w-[25%] border border-black h-screen"> </div>
     </div>
