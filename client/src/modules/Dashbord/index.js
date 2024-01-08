0e1599a4271ba626fc2cf9603a758fb72da5b7fb
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-08 23:58:14 +0530

    style profile

--- client/src/modules/Dashbord/index.js
+++ client/src/modules/Dashbord/index.js
@@ -5,8 +5,8 @@ const Dashbord = () => {
   return (
     <div className="w-screen flex">
         <div className="w-[25%] border border-black h-screen bg-secondary">
-            <div>
-                <img src=""/>
+            <div className="flex justify-center items-center">
+                <img src={Avatar} width={75} height={75}/>
                 <div>
                     <h3>Tutorial Dv</h3>
                     <p>my account</p>
