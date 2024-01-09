80258dc3ab6fd1190c6ebe8925cbdbedf9e3b62d
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-09 23:05:27 +0530

    profile style

--- client/src/modules/Dashbord/index.js
+++ client/src/modules/Dashbord/index.js
@@ -4,17 +4,17 @@ import Avatar from "../../assets/avatar.svg"
 const Dashbord = () => {
   return (
     <div className="w-screen flex">
-        <div className="w-[25%] border border-black h-screen bg-secondary">
-            <div className="flex justify-center items-center">
-                <img src={Avatar} width={75} height={75}/>
+        <div className="w-[25%]  h-screen bg-secondary">
+            <div className="flex justify-center items-center my-8">
+                <div className="border border-primary p-[2px] rounded-full"><img src={Avatar} width={75} height={75}/></div>
                 <div className="ml-8">
                     <h3 className="text-2xl">Tutorial Dv</h3>
-                    <p>my account</p>
+                    <p className="text-lg font-light">my account</p>
                 </div>
             </div>
         </div>
-        <div className="w-[50%] border border-black h-screen"> </div>
-        <div className="w-[25%] border border-black h-screen"> </div>
+        <div className="w-[50%] border h-screen"> </div>
+        <div className="w-[25%] border h-screen"> </div>
     </div>
    
   )
