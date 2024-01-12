4a61467d702cdffa72dec2ae3b8fb4f5e262820d
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-12 23:18:21 +0530

    type input style

--- client/src/modules/Dashbord/index.js
+++ client/src/modules/Dashbord/index.js
@@ -130,7 +130,7 @@ const Dashbord = () => {
         </div>
         <div className="p-14 w-full">
           <Input placeholder="type a message..." inputClassName="p-4 border-1 shadow-md rounded-full bg-light fouce:ring-0
-          " />
+         focus:border-0 outline-none " />
         </div>
       </div>
       <div className="w-[25%]  h-screen"> </div>
