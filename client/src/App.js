ac76f2686b6295228f4c6c42d35ad99c330716bc
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-14 23:25:33 +0530

    add route

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import Form from "./modules/Form";
 import Dashbord from "./modules/Dashbord";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    
     <div className="bg-[#edf3fc] h-screen flex justify-center items-center">
       <Dashbord />
       {/* <Form/> */}
