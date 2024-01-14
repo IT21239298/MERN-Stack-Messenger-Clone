873b3ff5fdc6c865b7d8c1615a10ab8c7ab7ce1a
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-14 23:58:20 +0530

    functiion protectRoutes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import Form from "./modules/Form";
 import Dashbord from "./modules/Dashbord";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, redirect } from "react-router-dom";
+
+const ProtectedRoutes = ({childen}) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null
+
+  if(!isLoggedIn) redirect('/users/sign_in')
+
+  return childen
+}
 
 function App() {
   return (
