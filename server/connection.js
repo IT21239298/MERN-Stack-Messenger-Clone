2d6962f065f62e83676dbe5ef29a7b6c81c82cc0
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-16 23:33:14 +0530

    create connection

---
+++ server/connection.js
@@ -0,0 +1,9 @@
+const mongoose  = require("mongoose")
+
+const url =`mongodb+srv://admin:admin123@cluster0.2oqp5b3.mongodb.net/`;
+
+mongoose.connect(url, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => console.log('Connected to DB')).catch((e) => console.log('error',e))
+
