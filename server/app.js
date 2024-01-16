1f031cfb05ef3709c94e084bbd230d5b69fff47a
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-16 23:24:24 +0530

    app.js

--- server/app.js
+++ server/app.js
@@ -8,9 +8,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req,res) => {
-    res.write('welcome')
+    res.send('welcome')
+    res.end()
 })
 
 app.listen(port, ()=>{
     console.log('listening on port ' + port)
-})
+ })
