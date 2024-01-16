203ad74b408eb48551f48e3bc3d26d1ce32cc429
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-16 23:27:52 +0530

    modify app.js

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req,res) => {
-    res.send('welcome')
-    res.end()
-})
+app.get("/", (req, res) => {
+  res.send("welcome");
+  res.end();
+});
 
-app.listen(port, ()=>{
-    console.log('listening on port ' + port)
- })
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
