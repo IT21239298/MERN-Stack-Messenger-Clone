dc93388105f7f45d2e3830951685a1289c321fd0
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-22 23:14:22 +0530

    remove maintenance import

--- client/src/modules/Dashbord/index.js
+++ client/src/modules/Dashbord/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Avatar from "../../assets/avatar.svg";
 import Input from "../../components/Input";
-import Maintenance from "../../components/maintenance";
+
 
 const Dashbord = () => {
   const contacts = [
