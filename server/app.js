cdd2591b1ee4eaf78a0ce4cccc9b436491c99c14
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-28 21:42:02 +0530

    api conversations

--- server/app.js
+++ server/app.js
@@ -95,8 +95,13 @@ app.post('/api/conversation',async (req, res) => {
   try{
 
     const {senderId, receiverId}  = req.body;
-    const newConversation = new Conversations({senderId, receiverId})
+    const newConversation = new Conversations({members:[senderId, receiverId]})
+    await newConversation.save();
+    res.status(200).send('Conversation created Successfully')
+    
   }catch(error){
+    console.log(error,"Error")
+
 
   }
 })
