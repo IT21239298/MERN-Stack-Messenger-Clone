714ff63423bb9a6b903622e37327ba116baebe72
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-02-01 00:06:10 +0530

    api post message update and validate

--- server/app.js
+++ server/app.js
@@ -140,25 +140,26 @@ app.get("/api/conversation/:userId", async (req, res) => {
   }
 });
 //post message
-app.post("/api/message", async (req, res) => {
+app.post('/api/message', async (req, res) => {
   try {
-    const { conversationId, senderId, message,receiverId = '' } = req.body;
-    if(!senderId || !message) return res.status(400).send('Please fill all required fields')
-    if(!conversationId){
-      const newConversation = new Conversations({members:[senderId, receiverId]});
-      await newConversation.save();
-      const newMessage = new Messages({conversationId: newConversation._id,senderId,message});
+      const { conversationId, senderId, message, receiverId = '' } = req.body;
+      if (!senderId || !message) return res.status(400).send('Please fill all required fields')
+      if (conversationId === 'new' && receiverId) {
+          const newCoversation = new Conversations({ members: [senderId, receiverId] });
+          await newCoversation.save();
+          const newMessage = new Messages({ conversationId: newCoversation._id, senderId, message });
+          await newMessage.save();
+          return res.status(200).send('Message sent successfully');
+      } else if (!conversationId && !receiverId) {
+          return res.status(400).send('Please fill all required fields')
+      }
+      const newMessage = new Messages({ conversationId, senderId, message });
       await newMessage.save();
-      return res.status(200).send('Message sent Successfully')
-    }
-
-    const newMessage = new Messages({ conversationId, senderId, message });
-    await newMessage.save();
-    res.status(200).send("Message sent Successfuly...!");
+      res.status(200).send('Message sent successfully');
   } catch (error) {
-    console.log(error, "Error");
+      console.log(error, 'Error')
   }
-});
+})
 
 //get message only users and messsage 
 app.get("/api/message/:conversationId", async (req, res) => {
