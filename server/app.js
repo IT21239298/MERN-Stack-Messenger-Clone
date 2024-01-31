42243fe59cb3504a38a5c0ded63d9be1634b0fc1
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-01-31 23:27:23 +0530

    api get users done

--- server/app.js
+++ server/app.js
@@ -171,7 +171,11 @@ app.get("/api/message/:conversationId", async (req, res) => {
 app.get('/api/users',async (req,res) => {
   try{
     const users= await Users.find();
-    
+    const usersData = Promise.all(users.map(async (user) => {
+      return {user: {email: user.email, fullName: user.fullName}, userId: user._id}
+    }))
+    res.status(200).json(await usersData)
+
 
   }catch(error){
         console.log("Error",error)
