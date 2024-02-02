88244aa957f49f1fdf4deded57791c3b63090084
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-02-03 00:00:03 +0530

    done register

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Form from "./modules/Form";
-import Dashbord from "./modules/Dashbord";
-import { Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+import Dashboard from './modules/Dashbord';
+import Form from './modules/Form';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ childen, auth = false }) => {
-  const isLoggedIn = localStorage.getItem("user:token") !== null || false;
+const ProtectedRoute = ({ children, auth=false }) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null || false;
 
-  if (!isLoggedIn && auth) {
-    return <Navigate to={"/users/sign_in"} />;
-  } else if (
-    isLoggedIn &&
-    ["/users/sign_in", "/users/sign_up"].includes(window.location.pathname)
-  ) {
-    return <Navigate to={"/"} />;
+  if(!isLoggedIn && auth) {
+    return <Navigate to={'/users/sign_in'} />
+  }else if(isLoggedIn && ['/users/sign_in', '/users/sign_up'].includes(window.location.pathname)){
+    console.log('object :>> ');
+    return <Navigate to={'/'} />
   }
 
-  return childen;
-};
+  return children
+}
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute auth={true}>
-          <Dashbord />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/users/sign_up"
-        element={
-         // <ProtectedRoute>
-            <Form isSignInPage={true} />
-        //  </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/users/sign_in"
-        element={
-        //  <ProtectedRoute>
-            <Form isSignInPage={false} />
-         // </ProtectedRoute>
-        }
-      />
+      <Route path='/' element={
+        <ProtectedRoute auth={true}>
+          <Dashboard/>
+        </ProtectedRoute>
+      } />
+      <Route path='/users/sign_in' element={
+      <ProtectedRoute>
+        <Form isSignInPage={true}/>
+      </ProtectedRoute>
+      } />
+      <Route path='/users/sign_up' element={
+        <ProtectedRoute>
+        <Form isSignInPage={false}/>
+      </ProtectedRoute>
+      } />
     </Routes>
   );
 }
--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -1,93 +1,57 @@
-import { useState } from "react";
-import Button from "../../components/Button";
-import Input from "../../components/Input";
-import { useNavigate} from "react-router-dom";
+import { useState } from "react"
+import Button from "../../components/Button"
+import Input from "../../components/Input"
+import { useNavigate } from 'react-router-dom'
 
-const Form = ({ isSignInPage = false }) => {
-  const [data, setData] = useState({
-    ...(!isSignInPage && {
-      fulName: "",
-    }),
-    email: "",
-    password: "",
-  });
-const navigate = useNavigate()
+const Form = ({
+    isSignInPage = true,
+}) => {
+    const [data, setData] = useState({
+        ...(!isSignInPage && {
+            fullName: ''
+        }),
+        email: '',
+        password: ''
+    })
+    const navigate = useNavigate()
 
-const handleSubmit = async(e) => {
-  console.log('data :>>', data);
-  e.preventDefault()
-  const res = await fetch(`http://localhost:8000/api/${isSignInPage ? 'login' : 'register'}`,{
-    method: 'POST',
-    headers: {
+    const handleSubmit = async(e) => {
+        console.log('data :>> ', data);
+        e.preventDefault()
+        const res = await fetch(`http://localhost:8000/api/${isSignInPage ? 'login' : 'register'}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
 
-      'Content-Type': 'application/json'
-    },
-    body:JSON.stringify(data)
-  })
-  const resData = await res.json()
-  console.log('data :>>', resData);
-
-}
-
+        if(res.status === 400) {
+            alert('Invalid credentials')
+        }else{
+            const resData = await res.json()
+            if(resData.token) {
+                localStorage.setItem('user:token', resData.token)
+                localStorage.setItem('user:detail', JSON.stringify(resData.user))
+                navigate('/')
+            }
+        }
+    }
   return (
     <div className="bg-light h-screen flex items-center justify-center">
-      <div className="bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
-        <div className=" text-4xl font-extrabold">
-          Welcome {isSignInPage && "Back"}
+        <div className=" bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
+            <div className=" text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
+            <div className=" text-xl font-light mb-14">{isSignInPage ? 'Sign in to get explored' : 'Sign up to get started'}</div>
+            <form className="flex flex-col items-center w-full" onSubmit={(e) => handleSubmit(e)}>
+            { !isSignInPage && <Input label="Full name" name="name" placeholder="Enter your full name" className="mb-6 w-[75%]" value={data.fullName} onChange={(e) => setData({ ...data, fullName: e.target.value }) } /> }
+            <Input label="Email address" type="email" name="email" placeholder="Enter your email" className="mb-6 w-[75%]" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value }) }/>
+            <Input label="Password" type="password" name="password" placeholder="Enter your Password" className="mb-14 w-[75%]" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value }) }/>
+            <Button label={isSignInPage ? 'Sign in': 'Sign up'} type='submit' className="w-[75%] mb-2" />
+            </form>
+            <div>{ isSignInPage ? "Didn't have an account?" : "Alredy have an account?"} <span className=" text-primary cursor-pointer underline" onClick={() => navigate(`/users/${isSignInPage ? 'sign_up' : 'sign_in'}`)}>{ isSignInPage ? 'Sign up' : 'Sign in'}</span></div>
         </div>
-        <div className=" text-xl font-light mb-14">
-          {isSignInPage
-            ? "Sign up to get started"
-            :  "Sign in to get explored"}
-        </div>
-        <form
-          className="flex flex-col items-center w-full"
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          {isSignInPage && (
-            <Input
-              label="Full Name"
-              name="name"
-              placeholder="Enter your name"
-              className="mb-6 w-[50%]"
-              isRequired
-              value={data.fulName}
-              onChange={(e) => setData({ ...data, fulName: e.target.value })}
-            />
-          )}
-          <Input
-            label="Email Address"
-            name="email"
-            type="email"
-            placeholder="Enter your email"
-            className="mb-6 "
-            isRequired
-            value={data.email}
-            onChange={(e) => setData({ ...data, email: e.target.value })}
-          />
-          <Input
-            label="Password"
-            type="password"
-            name="password"
-            placeholder="Enter your name"
-            className="mb-14"
-            value={data.password}
-            onChange={(e) => setData({ ...data, password: e.target.value })}
-          />
-        </form>
-        <Button
-          label={isSignInPage ? "Sign in" : "Sign up"}
-          className="w-1/2 mb-2"
-          type="submit"
-        />
-        <div>
-          {isSignInPage ? "Alredy have an account?" : "Didn't have an account?"}
-          <span className="text-primary cursor-pointer underline" onClick={() => navigate(`/users/${isSignInPage ? 'sign_in' : 'sign_up'}`)}>
-            {isSignInPage ?  "Sign in": "Sign up"}
-          </span>
-        </div>
-      </div>
     </div>
-  );
-};
-export default Form;
+  )
+}
+
+export default Form
