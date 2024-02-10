7ee3076a3d1b02b84f2f1bd8fff373ac6e8b4292
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-02-10 22:05:26 +0530

    implement dashbord featchure

--- client/src/modules/Dashbord/index.js
+++ client/src/modules/Dashbord/index.js
@@ -1,40 +1,44 @@
-import React, { useEffect, useState } from "react";
-import Avatar from "../../assets/avatar.svg";
+import { useEffect, useRef, useState } from "react";
+import Img1 from "../../assets/img1.jpg";
+import tutorialsdev from "../../assets/tutorialsdev.png";
 import Input from "../../components/Input";
+import { io } from "socket.io-client";
 
-const Dashbord = () => {
-  const contacts = [
-    {
-      name: "John",
-      status: "Availale",
-      img: Avatar,
-    },
-    {
-      name: "Mary",
-      status: "Availale",
-      img: Avatar,
-    },
-    {
-      name: "Alexander",
-      status: "Availale",
-      img: Avatar,
-    },
-    {
-      name: "Adam",
-      status: "Availale",
-      img: Avatar,
-    },
-    {
-      name: "Amal",
-      status: "Availale",
-      img: Avatar,
-    },
-    {
-      name: "Nipun ",
-      status: "Availale",
-      img: Avatar,
-    },
-  ];
+const Dashboard = () => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user:detail"))
+  );
+  const [conversations, setConversations] = useState([]);
+  const [messages, setMessages] = useState({});
+  const [message, setMessage] = useState("");
+  const [users, setUsers] = useState([]);
+  const [socket, setSocket] = useState(null);
+  const messageRef = useRef(null);
+
+  useEffect(() => {
+    setSocket(io("http://localhost:8080"));
+  }, []);
+
+  useEffect(() => {
+    socket?.emit("addUser", user?.id);
+    socket?.on("getUsers", (users) => {
+      console.log("activeUsers :>> ", users);
+    });
+    socket?.on("getMessage", (data) => {
+      setMessages((prev) => ({
+        ...prev,
+        messages: [
+          ...prev.messages,
+          { user: data.user, message: data.message },
+        ],
+      }));
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    messageRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages?.messages]);
+
   useEffect(() => {
     const loggedInUser = JSON.parse(localStorage.getItem("user:detail"));
     const fetchConversations = async () => {
@@ -52,56 +56,97 @@ const Dashbord = () => {
     };
     fetchConversations();
   }, []);
-  //get user details
-  const [user, setUser] = useState(
-    JSON.parse(localStorage.getItem("user:detail"))
-  );
-  const [conversations, setConversations] = useState([]);
-  const [messages,setMessages] = useState([])
-  console.log("User :>>", user);
-  console.log("conversations :>>", conversations);
 
-  const fetchMessage = async(conversationId) => {
-    const res = await fetch ( `http://localhost:8000/api/message/${conversationId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type' : 'application/json',
-      },
-      body:JSON.stringify({conversationId, senderId: user?.id,message:'Hello', receiverId:''})
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch(`http://localhost:8000/api/users/${user?.id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const resData = await res.json();
+      setUsers(resData);
+    };
+    fetchUsers();
+  }, []);
+
+  const fetchMessages = async (conversationId, receiver) => {
+    const res = await fetch(
+      `http://localhost:8000/api/message/${conversationId}?senderId=${user?.id}&&receiverId=${receiver?.receiverId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const resData = await res.json();
+    setMessages({ messages: resData, receiver, conversationId });
+  };
+
+  const sendMessage = async (e) => {
+    setMessage("");
+    socket?.emit("sendMessage", {
+      senderId: user?.id,
+      receiverId: messages?.receiver?.receiverId,
+      message,
+      conversationId: messages?.conversationId,
     });
-    const resData = await res.json()
-    console.log('resData :>>', resData)
-    setMessages(resData)
-  }
+    const res = await fetch(`http://localhost:8000/api/message`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        conversationId: messages?.conversationId,
+        senderId: user?.id,
+        message,
+        receiverId: messages?.receiver?.receiverId,
+      }),
+    });
+  };
 
   return (
     <div className="w-screen flex">
-      <div className="w-[25%]  h-screen bg-secondary">
+      <div className="w-[25%] h-screen bg-secondary overflow-scroll">
         <div className="flex items-center my-8 mx-14">
-          <div className="border border-primary p-[2px] rounded-full">
-            <img src={Avatar} width={75} height={75} />
+          <div>
+            <img
+              src={tutorialsdev}
+              width={75}
+              height={75}
+              className="border border-primary p-[2px] rounded-full"
+            />
           </div>
           <div className="ml-8">
-            <h3 className="text-2xl font-bold">{user.fullName}</h3>
-            <p className="text-lg  font-semibold">my account</p>
+            <h3 className="text-2xl">{user?.fullName}</h3>
+            <p className="text-lg font-light">My Account</p>
           </div>
         </div>
         <hr />
         <div className="mx-14 mt-10">
-          <div className="text-primary text-lg">Message</div>
+          <div className="text-primary text-lg">Messages</div>
           <div>
-            {!conversations.length > 0 ? (
-              conversations.map((conversationId, user) => {
-                console.log("conversation :>>", conversationId);
+            {conversations.length > 0 ? (
+              conversations.map(({ conversationId, user }) => {
                 return (
-                  <div className="flex  items-center py-8 border-b border-b-gray-300">
-                    <div className="cursor-pointer flex items-center">
-                      <div className=" cursor-pointer flex items-center" onClick={() => fetchMessage(conversationId)} >
-                        <img src={"img"} width={60} height={60} />
+                  <div className="flex items-center py-8 border-b border-b-gray-300">
+                    <div
+                      className="cursor-pointer flex items-center"
+                      onClick={() => fetchMessages(conversationId, user)}
+                    >
+                      <div>
+                        <img
+                          src={Img1}
+                          className="w-[60px] h-[60px] rounded-full p-[2px] border border-primary"
+                        />
                       </div>
-                      <div className="ml-8">
-                        <h3 className="text-2xl">{user?.fullName}</h3>
-                        <p className="text-lg font-light txt-gray-600">
+                      <div className="ml-6">
+                        <h3 className="text-lg font-semibold">
+                          {user?.fullName}
+                        </h3>
+                        <p className="text-sm font-light text-gray-600">
                           {user?.email}
                         </p>
                       </div>
@@ -110,119 +155,168 @@ const Dashbord = () => {
                 );
               })
             ) : (
-              <div className="'text-center txt-lg font-semibold mt-24">
-                No Conversation
+              <div className="text-center text-lg font-semibold mt-24">
+                No Conversations
               </div>
             )}
           </div>
         </div>
       </div>
       <div className="w-[50%] h-screen bg-white flex flex-col items-center">
-        <div className="w-[75%] bg-secondary h-[80px] mt-14 rounded-full flex items-center px-14 shadow-lg  ">
-          <div className="cursor-pointer">
-            <img src={Avatar} width={60} height={60} />
+        {messages?.receiver?.fullName && (
+          <div className="w-[75%] bg-secondary h-[80px] my-14 rounded-full flex items-center px-14 py-2">
+            <div className="cursor-pointer">
+              <img src={Img1} width={60} height={60} className="rounded-full" />
+            </div>
+            <div className="ml-6 mr-auto">
+              <h3 className="text-lg">{messages?.receiver?.fullName}</h3>
+              <p className="text-sm font-light text-gray-600">
+                {messages?.receiver?.email}
+              </p>
+            </div>
+            <div className="cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-phone-outgoing"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="black"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                <line x1="15" y1="9" x2="20" y2="4" />
+                <polyline points="16 4 20 4 20 8" />
+              </svg>
+            </div>
           </div>
-          <div className="ml-6 mr-auto">
-            <h3 className="text-lg ">Alexander</h3>
-            <p className="text-sm font-light text-gray-600">online</p>
-          </div>
-          <div className="cursor-pointer shadow-lg rounded-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-phone-outgoing"
-              width="22"
-              height="22"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="black"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-              <path d="M15 9l5 -5" />
-              <path d="M16 4l4 0l0 4" />
-            </svg>
+        )}
+        <div className="h-[75%] w-full overflow-scroll shadow-sm">
+          <div className="p-14">
+            {messages?.messages?.length > 0 ? (
+              messages.messages.map(({ message, user: { id } = {} }) => {
+                return (
+                  <>
+                    <div
+                      className={`max-w-[40%] rounded-b-xl p-4 mb-6 ${
+                        id === user?.id
+                          ? "bg-primary text-white rounded-tl-xl ml-auto"
+                          : "bg-secondary rounded-tr-xl"
+                      } `}
+                    >
+                      {message}
+                    </div>
+                    <div ref={messageRef}></div>
+                  </>
+                );
+              })
+            ) : (
+              <div className="text-center text-lg font-semibold mt-24">
+                No Messages or No Conversation Selected
+              </div>
+            )}
           </div>
         </div>
-        <div className="h-[75%] w-full overflow-y-auto border-b ">
-          <div className=" p-14 shadow-lg">
-            <div className="h-[80px] w-[300px] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6 ">
-              sdibuifbfibflbweiubfwibbbfeifbfibffbw wefbweifbweif wefbifb
-              iwebfwifb we
-            </div>
-            <div className="h-[80px] w-[300px] bg-primary rounded-b-xl rounded-tl-xl ml-auto p-4 text-white">
-              sdibuifbfibflbweiubfwibbbfeifbfibffbw wefbweifbweif wefbifb
-              iwebfwifb we
-            </div>
-            <div className="h-[80px] w-[300px] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              sdibuifbfibflbweiubfwibbbfeifbfibffbw wefbweifbweif wefbifb
-              iwebfwifb we
-            </div>
-            <div className="h-[80px] w-[300px] bg-primary rounded-b-xl rounded-tl-xl ml-auto p-4 text-white">
-              sdibuifbfibflbweiubfwibbbfeifbfibffbw wefbweifbweif wefbifb
-              iwebfwifb we
-            </div>
-            <div className="h-[80px] w-[300px] bg-secondary rounded-b-xl rounded-tr-xl p-4 mb-6">
-              sdibuifbfibflbweiubfwibbbfeifbfibffbw wefbweifbweif wefbifb
-              iwebfwifb we
-            </div>
-            <div className="h-[80px] w-[300px] bg-primary rounded-b-xl rounded-tl-xl ml-auto p-4 text-white">
-              sdibuifbfibflbweiubfwibbbfeifbfibffbw wefbweifbweif wefbifb
-              iwebfwifb we
-            </div>
-          </div>
-        </div>
-        <div className="p-14 w-full flex items-center">
-          <Input
-            placeholder="type a message..."
-            className="w-[75%]"
-            inputClassName="p-4 border-1 shadow-md rounded-full bg-light fouce:ring-0
-         focus:border-0 outline-none "
-          />
-          <div className="ml-4 p-2 cursor-pointer bg-light rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-send"
-              width="30"
-              height="30"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#2c3e50"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+        {messages?.receiver?.fullName && (
+          <div className="p-14 w-full flex items-center">
+            <Input
+              placeholder="Type a message..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-[75%]"
+              inputClassName="p-4 border-0 shadow-md rounded-full bg-light focus:ring-0 focus:border-0 outline-none"
+            />
+            <div
+              className={`ml-4 p-2 cursor-pointer bg-light rounded-full ${
+                !message && "pointer-events-none"
+              }`}
+              onClick={() => sendMessage()}
             >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M10 14l11 -11" />
-              <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
-            </svg>
-          </div>
-          <div className="ml-4 p-2 cursor-pointer bg-light rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-circle-plus"
-              width="30"
-              height="30"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#2c3e50"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-send"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#2c3e50"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <line x1="10" y1="14" x2="21" y2="3" />
+                <path d="M21 3l-6.5 18a0.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a0.55 .55 0 0 1 0 -1l18 -6.5" />
+              </svg>
+            </div>
+            <div
+              className={`ml-4 p-2 cursor-pointer bg-light rounded-full ${
+                !message && "pointer-events-none"
+              }`}
             >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-              <path d="M9 12h6" />
-              <path d="M12 9v6" />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-circle-plus"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#2c3e50"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <circle cx="12" cy="12" r="9" />
+                <line x1="9" y1="12" x2="15" y2="12" />
+                <line x1="12" y1="9" x2="12" y2="15" />
+              </svg>
+            </div>
           </div>
+        )}
+      </div>
+      <div className="w-[25%] h-screen bg-light px-8 py-16 overflow-scroll">
+        <div className="text-primary text-lg">People</div>
+        <div>
+          {users.length > 0 ? (
+            users.map(({ userId, user }) => {
+              return (
+                <div className="flex items-center py-8 border-b border-b-gray-300">
+                  <div
+                    className="cursor-pointer flex items-center"
+                    onClick={() => fetchMessages("new", user)}
+                  >
+                    <div>
+                      <img
+                        src={Img1}
+                        className="w-[60px] h-[60px] rounded-full p-[2px] border border-primary"
+                      />
+                    </div>
+                    <div className="ml-6">
+                      <h3 className="text-lg font-semibold">
+                        {user?.fullName}
+                      </h3>
+                      <p className="text-sm font-light text-gray-600">
+                        {user?.email}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <div className="text-center text-lg font-semibold mt-24">
+              No Conversations
+            </div>
+          )}
         </div>
       </div>
-      <div className="w-[25%]  h-screen"> </div>
     </div>
   );
 };
 
-export default Dashbord;
+export default Dashboard;
